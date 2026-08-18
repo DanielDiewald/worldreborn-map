7c3f1716ffdfd431ce675af94b49854d08b8bb68
Daniel Diewald <72552167+DanielDiewald@users.noreply.github.com>
2026-08-18 14:26:24 +0200

    Add standalone fantasy calendar SQL entrypoint

---
+++ scripts/fantasy-calendar-migration.sql
@@ -0,0 +1,4 @@
+\set ON_ERROR_STOP on
+\echo 'Applying WorldReborn fantasy calendar/media migration 0006...'
+\ir ../migrations/0006_fantasy_calendar_media_crud.sql
+\echo 'Fantasy calendar migration applied.'
