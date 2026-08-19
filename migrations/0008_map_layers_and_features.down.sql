f7feca0d920e2deaab8e1ec2c092bc55f56d8876
Daniel Diewald <72552167+DanielDiewald@users.noreply.github.com>
2026-08-19 18:26:57 +0200

    Add rollback for map layers and vector features

---
+++ migrations/0008_map_layers_and_features.down.sql
@@ -0,0 +1,6 @@
+BEGIN;
+DROP TABLE IF EXISTS public.map_feature_visibility;
+DROP TABLE IF EXISTS public.map_features;
+DROP TABLE IF EXISTS public.map_layer_visibility;
+DROP TABLE IF EXISTS public.project_map_layers;
+COMMIT;
